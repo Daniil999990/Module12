585e550db86fa259bc78d58d24b8924ad5a4c8d1
Daniil999990 <dannydendi@gmail.com>
2023-04-21 19:34:47 +0300

    FizzBuzzMultithreaded

---
+++ FizzBuzzMultithreaded/FizzBuzzMultithreaded.java
@@ -0,0 +1,92 @@
+package FizzBuzzMultithreaded;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class FizzBuzzMultithreaded {
+
+    private final int n;
+    private final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+
+    public FizzBuzzMultithreaded(int n) {
+        this.n = n;
+    }
+
+    public void fizz() throws InterruptedException {
+        for (int i = 3; i <= n; i += 3) {
+            if (i % 5 != 0) {
+                queue.put("fizz");
+            }
+        }
+    }
+
+    public void buzz() throws InterruptedException {
+        for (int i = 5; i <= n; i += 5) {
+            if (i % 3 != 0) {
+                queue.put("buzz");
+            }
+        }
+    }
+
+    public void fizzbuzz() throws InterruptedException {
+        for (int i = 15; i <= n; i += 15) {
+            queue.put("fizzbuzz");
+        }
+    }
+
+    public void number() throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(queue.take() + " ");
+            } else if (i % 3 == 0) {
+                System.out.print(queue.take() + " ");
+            } else if (i % 5 == 0) {
+                System.out.print(queue.take() + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+        FizzBuzzMultithreaded fizzBuzz = new FizzBuzzMultithreaded(n);
+
+        Thread threadA = new Thread(() -> {
+            try {
+                fizzBuzz.fizz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            try {
+                fizzBuzz.buzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadC = new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadD = new Thread(() -> {
+            try {
+                fizzBuzz.number();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+    }
+}
