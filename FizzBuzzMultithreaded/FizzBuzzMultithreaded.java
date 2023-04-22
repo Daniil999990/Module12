addfdc45c631d5042966d0403088699633d8e640
Daniil999990 <dannydendi@gmail.com>
2023-04-22 23:57:19 +0300

    FizzBuzzMultithreaded

--- FizzBuzzMultithreaded/FizzBuzzMultithreaded.java
+++ FizzBuzzMultithreaded/FizzBuzzMultithreaded.java
@@ -5,84 +5,66 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class FizzBuzzMultithreaded {
 
-    private final int n;
-    private final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+    private int n;
+    private int currentNumber;
+    private BlockingQueue<String> queue;
 
     public FizzBuzzMultithreaded(int n) {
         this.n = n;
+        this.currentNumber = 1;
+        this.queue = new LinkedBlockingQueue<>();
     }
 
-    public void fizz() throws InterruptedException {
-        for (int i = 3; i <= n; i += 3) {
-            if (i % 5 != 0) {
-                queue.put("fizz");
+    public void fizz() {
+        while (currentNumber <= n) {
+            if (currentNumber % 3 == 0 && currentNumber % 5 != 0) {
+                queue.add("fizz");
             }
+            currentNumber++;
         }
     }
 
-    public void buzz() throws InterruptedException {
-        for (int i = 5; i <= n; i += 5) {
-            if (i % 3 != 0) {
-                queue.put("buzz");
+    public void buzz() {
+        while (currentNumber <= n) {
+            if (currentNumber % 5 == 0 && currentNumber % 3 != 0) {
+                queue.add("buzz");
             }
+            currentNumber++;
         }
     }
 
-    public void fizzbuzz() throws InterruptedException {
-        for (int i = 15; i <= n; i += 15) {
-            queue.put("fizzbuzz");
+    public void fizzbuzz() {
+        while (currentNumber <= n) {
+            if (currentNumber % 3 == 0 && currentNumber % 5 == 0) {
+                queue.add("fizzbuzz");
+            }
+            currentNumber++;
         }
     }
 
-    public void number() throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print(queue.take() + " ");
-            } else if (i % 3 == 0) {
-                System.out.print(queue.take() + " ");
-            } else if (i % 5 == 0) {
-                System.out.print(queue.take() + " ");
-            } else {
-                System.out.print(i + " ");
+    public void number() {
+        while (currentNumber <= n) {
+            try {
+                String value = queue.take();
+                System.out.print(value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+
+            if (currentNumber <= n) {
+                System.out.print(", ");
+            }
+            currentNumber++;
         }
     }
 
     public static void main(String[] args) {
-        int n = 15;
-        FizzBuzzMultithreaded fizzBuzz = new FizzBuzzMultithreaded(n);
+        FizzBuzzMultithreaded fizzBuzz = new FizzBuzzMultithreaded(15);
 
-        Thread threadA = new Thread(() -> {
-            try {
-                fizzBuzz.fizz();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        Thread threadB = new Thread(() -> {
-            try {
-                fizzBuzz.buzz();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        Thread threadC = new Thread(() -> {
-            try {
-                fizzBuzz.fizzbuzz();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        Thread threadD = new Thread(() -> {
-            try {
-                fizzBuzz.number();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        Thread threadA = new Thread(fizzBuzz::fizz);
+        Thread threadB = new Thread(fizzBuzz::buzz);
+        Thread threadC = new Thread(fizzBuzz::fizzbuzz);
+        Thread threadD = new Thread(fizzBuzz::number);
 
         threadA.start();
         threadB.start();
